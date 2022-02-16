2cbde2f619a263c0e2bef0d18204eba0ddca3ccd
Andrea <ngocanhhsgs@gmail.com>
2022-02-16 21:01:29 +0200

    fix style

--- front-end/src/component/navbar/NavBar.tsx
+++ front-end/src/component/navbar/NavBar.tsx
@@ -175,7 +175,7 @@ export default function PrimarySearchAppBar() {
             component="div"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
-            EXPLORE HELSINKI
+            HELSINKI
           </Typography>
           <Search>
             <SearchIconWrapper>
